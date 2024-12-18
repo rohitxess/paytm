1adac03a5a9699b5f3887ed16c4bca927f21a4df
rohitxess <xessro@gmail.com>
2024-12-18 11:48:23 +0530

    created signin routes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -22,7 +22,7 @@ router.post("/signup", async (res, req) => {
     const {success} = signupBody.safeParse(req.body)
     if (!success){
         return res.status (411).json({
-            message: "Email alreadt taken / Incorrect inputs"
+            message: "Email already taken / Incorrect inputs"
         })
     }
 
@@ -53,6 +53,46 @@ router.post("/signup", async (res, req) => {
         token: token
     })
 })
+
+// writting the zod validation for signin route 
+
+const signinBody = zod.object({
+    username: zod.string().email,
+    password: zod.string()
+})
+
+router.post ("/signin", async (res, req) => {
+    const {success} = signinBody.safeParse(req.body)
+    if (!success){
+        return res.status(411).json ({
+            message: "Incorrect inputs"
+        })
+    }
+
+    const user = await User.findOne({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    if (user){
+        const token = jwt.sign({
+            userId: user._id
+        }, JWT_SECRET);
+
+        res.json({
+            token: token
+        })
+        return ;
+    }
+
+    res.status(411).json({
+        message:"Error while logging in"
+    })
+
+})
+
+
+
 
 module.exports = router;
 
